ac6bf311e2ce330415894afde04e739bbcabce7d
boostup <boostup@gmail.com>
2020-09-11 16:03:13 +0200

    active navlinks are behaving as expected

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import { Route, Switch, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 import Nav from "./components/Nav";
+
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import Threeeee from "./pages/threeeee";
 import About from "./pages/about";
+import Four04 from "./pages/404";
 
 export default () => {
   const location = useLocation();
+
   return (
     <>
       <Nav />
       <AnimatePresence exitBeforeEnter>
         <Switch key={location.key} location={location}>
+          <Route exact path="/" component={Home} />
           <Route path="/contact" component={Contact} />
           <Route path="/threeeee" component={Threeeee} />
           <Route path="/about" component={About} />
-          <Route path="/" component={Home} />
+          <Route component={Four04} />
         </Switch>
       </AnimatePresence>
     </>
